6346643e2992dfb497053f3c3193d97ded7834c9
Kevin De Rosa <kevinderosa@Kevins-MBP-2.domain>
2019-07-20 14:48:09 -0700

    updated files

--- main.js
+++ main.js
@@ -18,22 +18,43 @@ function myFunction() {
   
 }
 
-//Menu Navbar Code
 
-// window.onscroll = function() {myFunction()};
 
-// let navbar = document.getElementById("menuNavbar");
-// let sticky = navbar.offsetTop;
-// console.log(sticky)
+const firebaseConfig = {
+  apiKey: "AIzaSyCF5khD8A_rM9hYhdhUdcRP0F9L8nPkmv8",
+  authDomain: "briemail-48eb7.firebaseapp.com",
+  databaseURL: "https://briemail-48eb7.firebaseio.com",
+  projectId: "briemail-48eb7",
+  storageBucket: "",
+  messagingSenderId: "942795508931",
+  appId: "1:942795508931:web:a470fa94b097b80c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky");
+const database = firebase.database()
+
+$("#addEmail").on("click", function(event){
+  event.preventDefault();
+
+  let custName = $("#name").val().trim();
+  let custEmail = $("#email").val().trim();
   
-    
-//   } else {
-//     navbar.classList.remove("sticky");
- 
-//   }
   
-// }
+  console.log(custName);
+  console.log(custEmail);
+
+
+  let newEmail = {
+      Name:custName,
+      Email:custEmail,    
+  }
+
+database.ref().push(newEmail);
+
+$("#name").val("Thanks for signing up!");
+$("#email").val(" ");
+
+})
+
+
